ad2c7335113395455074f38ff72457cb8d9cd1d3
Andy <almagambetova.aidana01@gmail.com>
2025-10-28 20:16:04 +0500

    create prisma.service

--- server/src/posts/posts.service.ts
+++ server/src/posts/posts.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly prisma: PrismaService) {}
+}
