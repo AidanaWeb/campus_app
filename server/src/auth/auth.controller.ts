ab1cf2faa75ff5ae72aa080a9fa579e6a8e7e443
Andy <almagambetova.aidana01@gmail.com>
2025-10-30 22:13:44 +0500

    added auth/dtos, auth signup endpoint

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { SignupDto } from "./dtos/signup.dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post("signup")
+  async signUp(@Body() signupData: SignupDto) {}
 }
---
+++ server/src/auth/dtos/signup.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsString, Matches, MinLength } from "class-validator";
+
+export class SignupDto {
+  @IsString({ message: "Введите некорректные данные" })
+  @MinLength(2, { message: "Имя должно содержать хотя бы 2 символа" })
+  name: string;
+
+  @IsEmail({}, { message: "Введите корректный email" })
+  email: string;
+
+  @IsString({ message: "Введите некорректные данные" })
+  @MinLength(6, { message: "Пароль должен содержать минимум 6 символов" })
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@#$%^&*!]{6,}$/, {
+    message: "Пароль должен содержать хотя бы 1 цифру и 1 символ",
+  })
+  password: string;
+}
