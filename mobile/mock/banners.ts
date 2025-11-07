f1a691466602a8da6f9421a596e3859d13f062d2
Andy <almagambetova.aidana01@gmail.com>
2025-11-07 17:49:17 +0500

    mobile/added mock/ folder

---
+++ mobile/mock/banners.ts
@@ -0,0 +1,37 @@
+export const banners = [
+  {
+    id: 1,
+    title: "Осенний фестиваль в университете",
+    image:
+      "https://images.unsplash.com/photo-1503676260728-1c00da094a0b?w=800&h=500&fit=crop",
+    link: "/events/festival",
+  },
+  {
+    id: 2,
+    title: "Новые возможности для студентов: Erasmus+",
+    image:
+      "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=800&h=500&fit=crop",
+    link: "/news/erasmus",
+  },
+  {
+    id: 3,
+    title: "Киберспорт в нашем универе 🎮",
+    image:
+      "https://images.unsplash.com/photo-1606112219348-204d7d8b94ee?w=800&h=500&fit=crop",
+    link: "/clubs/esports",
+  },
+  {
+    id: 4,
+    title: "IT-хакатон: покажи свои навыки",
+    image:
+      "https://images.unsplash.com/photo-1522199710521-72d69614c702?w=800&h=500&fit=crop",
+    link: "/events/hackathon",
+  },
+  {
+    id: 5,
+    title: "Открылся новый коворкинг в главном корпусе",
+    image:
+      "https://images.unsplash.com/photo-1521737604893-d14cc237f11d?w=800&h=500&fit=crop",
+    link: "/news/coworking",
+  },
+];
