ab30d8285a889de7c85ae600bd065ddac46d0199
Andy <almagambetova.aidana01@gmail.com>
2025-11-08 16:12:29 +0500

    added buttons in home scr

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import Carousel from "@/components/Carousel";
 
 import { banners } from "@/mock/banners";
 import AppText from "@/components/AppText";
+import Button from "@/components/Button";
 const { width } = Dimensions.get("window");
 
 export default function TabOneScreen() {
@@ -16,6 +17,26 @@ export default function TabOneScreen() {
 }
 
 const ListHeader = () => {
+  const filterButtons = [
+    {
+      id: 1,
+      title: "Все",
+      isActive: true,
+    },
+    {
+      id: 2,
+      title: "События",
+    },
+    {
+      id: 3,
+      title: "Новости",
+    },
+    {
+      id: 4,
+      title: "Подписки",
+    },
+  ];
+
   return (
     <View style={styles.headerContainer}>
       <AppText
@@ -28,14 +49,26 @@ const ListHeader = () => {
 
       <Carousel data={banners} />
 
-      
+      <FlatList
+        data={filterButtons}
+        renderItem={({ item }) => (
+          <Button title={item.title} isActive={item.isActive} />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{
+          marginLeft: 20,
+          marginTop: 10,
+        }}
+        ItemSeparatorComponent={() => <View style={{ width: 10 }} />}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   headerContainer: {
-    gap: 20,
+    gap: 10,
     marginTop: 10,
   },
 });
