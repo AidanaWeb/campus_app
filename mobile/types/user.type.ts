3261e375d536c47d1f1193344780afb2d197e09b
Andy <almagambetova.aidana01@gmail.com>
2025-11-09 17:19:11 +0500

    added mock users

---
+++ mobile/types/user.type.ts
@@ -0,0 +1,14 @@
+export type UserRole = "STUDENT" | "TEACHER" | "ADMIN";
+
+export interface User {
+  id: string;
+  name: string;
+  lastName?: string;
+  avatar?: string;
+  email: string;
+  bio?: string;
+  phone?: string;
+  role: UserRole;
+  createdAt: Date;
+  updatedAt: Date;
+}
