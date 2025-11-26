bf85155753ca7f45be9f2cd8a650fb847d7069e4
Andy <almagambetova.aidana01@gmail.com>
2025-11-26 18:26:20 +0500

    changed ip

--- mobile/config/api.ts
+++ mobile/config/api.ts
@@ -1,3 +1,2 @@
 export const API_VERSION = "api/v1";
-// export const API_BASE_URL = "http://localhost:3000/" + API_VERSION;
-export const API_BASE_URL = "http://192.168.0.62:3000/" + API_VERSION;
+export const API_BASE_URL = "http://192.168.0.71:3000/" + API_VERSION;
